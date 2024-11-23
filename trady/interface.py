945f245b21cd1f734f153464149aecc19255bd5c
Ernest <ernest@xeioka.me>
2024-11-23 17:39:25 +0700

    Binance: retrieve date & time.

---
+++ trady/interface.py
@@ -0,0 +1,30 @@
+"""This module contains the abstract exchange interface.
+
+All exchange implementations must subclass `ExchangeInterface` and implement required
+methods by utilizing the underlying `session` for making HTTP requests (API calls).
+
+Third-party API connectors/libraries _must not_ be used
+since they tend to restrict the available functionality.
+"""
+
+import abc
+from datetime import datetime
+
+from requests import Session
+
+
+class ExchangeInterface(abc.ABC):
+    """Abstract exchange interface.
+
+    Attributes
+    ----------
+    session: Session
+        Requests session. Must be used for _all_ HTTP requests.
+    """
+
+    def __init__(self) -> None:
+        self.session: Session = Session()
+
+    def get_datetime(self) -> datetime:
+        """Retrieves date and time."""
+        raise NotImplementedError
