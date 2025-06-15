6d05dc26134ea8262d44b388fad3fee985c076b7
Ernest <ernest@xeioka.me>
2025-06-15 16:05:20 +0800

    Refactor imports.

--- trady/__init__.py
+++ trady/__init__.py
@@ -1,32 +1,10 @@
 """A single interface for various exchanges."""
 
-from .datatypes import (
-    AccountPositions,
-    Balance,
-    Candlestick,
-    MarketRules,
-    MarketStats,
-    Position,
-    Symbol,
-    SymbolRules,
-    SymbolStats,
-)
-from .exceptions import ExchangeException
 from .exchanges import Binance
 from .interface import ExchangeInterface
 
 
 __all__ = (
-    "AccountPositions",
-    "Balance",
-    "Candlestick",
-    "MarketRules",
-    "MarketStats",
-    "Position",
-    "Symbol",
-    "SymbolRules",
-    "SymbolStats",
-    "ExchangeException",
     "Binance",
     "ExchangeInterface",
 )
--- trady/exchanges/binance/__init__.py
+++ trady/exchanges/binance/__init__.py
@@ -5,8 +5,3 @@ Resources
 API documentation:
     - https://developers.binance.com/docs/derivatives/usds-margined-futures/general-info
 """
-
-from .interface import Binance
-
-
-__all__ = ("Binance",)
